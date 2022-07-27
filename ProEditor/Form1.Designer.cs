8d50c12401b23e90d4e7af1d2ee520de772fcefd
Алексей Парусников <instrpro@mail.ru>
2022-07-27 12:46:07 +0600

    Сборка

---
+++ ProEditor/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿
+namespace ProEditor
+{
+  partial class frm_edit_prog
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_edit_prog));
+      this.lab_lst_prog = new System.Windows.Forms.Label();
+      this.txt_box_prog = new System.Windows.Forms.TextBox();
+      this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+      this.but_save = new System.Windows.Forms.Button();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.label6 = new System.Windows.Forms.Label();
+      this.txt_D = new System.Windows.Forms.TextBox();
+      this.txt_XY = new System.Windows.Forms.TextBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.txt_G = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.txt_H2 = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.txt_H1 = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.txt_H = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.but_edit = new System.Windows.Forms.Button();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.txt_Z2 = new System.Windows.Forms.TextBox();
+      this.label7 = new System.Windows.Forms.Label();
+      this.txt_Z1 = new System.Windows.Forms.TextBox();
+      this.label8 = new System.Windows.Forms.Label();
+      this.txt_F = new System.Windows.Forms.TextBox();
+      this.label9 = new System.Windows.Forms.Label();
+      this.txt_S = new System.Windows.Forms.TextBox();
+      this.label10 = new System.Windows.Forms.Label();
+      this.but_close = new System.Windows.Forms.Button();
+      this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // lab_lst_prog
+      // 
+      this.lab_lst_prog.AutoSize = true;
+      this.lab_lst_prog.Location = new System.Drawing.Point(9, 260);
+      this.lab_lst_prog.Name = "lab_lst_prog";
+      this.lab_lst_prog.Size = new System.Drawing.Size(99, 13);
+      this.lab_lst_prog.TabIndex = 2;
+      this.lab_lst_prog.Text = "Текст программы";
+      // 
+      // txt_box_prog
+      // 
+      this.txt_box_prog.Location = new System.Drawing.Point(12, 276);
+      this.txt_box_prog.Multiline = true;
+      this.txt_box_prog.Name = "txt_box_prog";
+      this.txt_box_prog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.txt_box_prog.Size = new System.Drawing.Size(383, 394);
+      this.txt_box_prog.TabIndex = 4;
+      // 
+      // but_save
+      // 
+      this.but_save.Location = new System.Drawing.Point(93, 676);
+      this.but_save.Name = "but_save";
+      this.but_save.Size = new System.Drawing.Size(75, 23);
+      this.but_save.TabIndex = 5;
+      this.but_save.Text = "Сохранить";
+      this.but_save.UseVisualStyleBackColor = true;
+      this.but_save.Click += new System.EventHandler(this.but_save_Click);
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.label6);
+      this.groupBox1.Controls.Add(this.txt_D);
+      this.groupBox1.Controls.Add(this.txt_XY);
+      this.groupBox1.Controls.Add(this.label5);
+      this.groupBox1.Controls.Add(this.txt_G);
+      this.groupBox1.Controls.Add(this.label4);
+      this.groupBox1.Controls.Add(this.txt_H2);
+      this.groupBox1.Controls.Add(this.label3);
+      this.groupBox1.Controls.Add(this.txt_H1);
+      this.groupBox1.Controls.Add(this.label2);
+      this.groupBox1.Controls.Add(this.txt_H);
+      this.groupBox1.Controls.Add(this.label1);
+      this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.groupBox1.Location = new System.Drawing.Point(12, 12);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(393, 157);
+      this.groupBox1.TabIndex = 6;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Параметры цикла G83";
+      this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.Location = new System.Drawing.Point(6, 130);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(125, 13);
+      this.label6.TabIndex = 11;
+      this.label6.Text = "Диаметр отверстий - D";
+      // 
+      // txt_D
+      // 
+      this.txt_D.Location = new System.Drawing.Point(146, 127);
+      this.txt_D.Name = "txt_D";
+      this.txt_D.Size = new System.Drawing.Size(50, 20);
+      this.txt_D.TabIndex = 10;
+      this.txt_D.Text = "6";
+      // 
+      // txt_XY
+      // 
+      this.txt_XY.Location = new System.Drawing.Point(216, 42);
+      this.txt_XY.Multiline = true;
+      this.txt_XY.Name = "txt_XY";
+      this.txt_XY.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+      this.txt_XY.Size = new System.Drawing.Size(167, 101);
+      this.txt_XY.TabIndex = 9;
+      this.txt_XY.Text = "X100 Y-200\r\nX-100 Y200";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(213, 23);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(86, 13);
+      this.label5.TabIndex = 8;
+      this.label5.Text = "Координаты XY";
+      // 
+      // txt_G
+      // 
+      this.txt_G.Location = new System.Drawing.Point(146, 101);
+      this.txt_G.Name = "txt_G";
+      this.txt_G.Size = new System.Drawing.Size(50, 20);
+      this.txt_G.TabIndex = 7;
+      this.txt_G.Text = "20";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.Location = new System.Drawing.Point(6, 104);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(102, 13);
+      this.label4.TabIndex = 6;
+      this.label4.Text = "Общая глубина - G";
+      // 
+      // txt_H2
+      // 
+      this.txt_H2.Location = new System.Drawing.Point(146, 75);
+      this.txt_H2.Name = "txt_H2";
+      this.txt_H2.Size = new System.Drawing.Size(50, 20);
+      this.txt_H2.TabIndex = 5;
+      this.txt_H2.Text = "1";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(6, 78);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(73, 13);
+      this.label3.TabIndex = 4;
+      this.label3.Text = "Недоход - H2";
+      // 
+      // txt_H1
+      // 
+      this.txt_H1.Location = new System.Drawing.Point(146, 49);
+      this.txt_H1.Name = "txt_H1";
+      this.txt_H1.Size = new System.Drawing.Size(50, 20);
+      this.txt_H1.TabIndex = 3;
+      this.txt_H1.Text = "3";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(6, 52);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(99, 13);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "Глубина шага - H1";
+      // 
+      // txt_H
+      // 
+      this.txt_H.Location = new System.Drawing.Point(146, 23);
+      this.txt_H.Name = "txt_H";
+      this.txt_H.Size = new System.Drawing.Size(50, 20);
+      this.txt_H.TabIndex = 1;
+      this.txt_H.Text = "5";
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(6, 26);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(119, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Начальная высота - H";
+      // 
+      // but_edit
+      // 
+      this.but_edit.Location = new System.Drawing.Point(12, 676);
+      this.but_edit.Name = "but_edit";
+      this.but_edit.Size = new System.Drawing.Size(75, 23);
+      this.but_edit.TabIndex = 7;
+      this.but_edit.Text = "Выполнить";
+      this.but_edit.UseVisualStyleBackColor = true;
+      this.but_edit.Click += new System.EventHandler(this.but_edit_Click);
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.txt_Z2);
+      this.groupBox2.Controls.Add(this.label7);
+      this.groupBox2.Controls.Add(this.txt_Z1);
+      this.groupBox2.Controls.Add(this.label8);
+      this.groupBox2.Controls.Add(this.txt_F);
+      this.groupBox2.Controls.Add(this.label9);
+      this.groupBox2.Controls.Add(this.txt_S);
+      this.groupBox2.Controls.Add(this.label10);
+      this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.groupBox2.Location = new System.Drawing.Point(12, 175);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(393, 81);
+      this.groupBox2.TabIndex = 8;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "Параметры программы";
+      // 
+      // txt_Z2
+      // 
+      this.txt_Z2.Location = new System.Drawing.Point(333, 49);
+      this.txt_Z2.Name = "txt_Z2";
+      this.txt_Z2.Size = new System.Drawing.Size(50, 20);
+      this.txt_Z2.TabIndex = 7;
+      this.txt_Z2.Text = "10";
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.Location = new System.Drawing.Point(213, 52);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(109, 13);
+      this.label7.TabIndex = 6;
+      this.label7.Text = "Высота в конце - Z2";
+      // 
+      // txt_Z1
+      // 
+      this.txt_Z1.Location = new System.Drawing.Point(333, 23);
+      this.txt_Z1.Name = "txt_Z1";
+      this.txt_Z1.Size = new System.Drawing.Size(50, 20);
+      this.txt_Z1.TabIndex = 5;
+      this.txt_Z1.Text = "20";
+      // 
+      // label8
+      // 
+      this.label8.AutoSize = true;
+      this.label8.Location = new System.Drawing.Point(213, 23);
+      this.label8.Name = "label8";
+      this.label8.Size = new System.Drawing.Size(114, 13);
+      this.label8.TabIndex = 4;
+      this.label8.Text = "Высота в начале - Z1";
+      // 
+      // txt_F
+      // 
+      this.txt_F.Location = new System.Drawing.Point(146, 49);
+      this.txt_F.Name = "txt_F";
+      this.txt_F.Size = new System.Drawing.Size(50, 20);
+      this.txt_F.TabIndex = 3;
+      this.txt_F.Text = "70";
+      // 
+      // label9
+      // 
+      this.label9.AutoSize = true;
+      this.label9.Location = new System.Drawing.Point(6, 49);
+      this.label9.Name = "label9";
+      this.label9.Size = new System.Drawing.Size(59, 13);
+      this.label9.TabIndex = 2;
+      this.label9.Text = "Подача - F";
+      // 
+      // txt_S
+      // 
+      this.txt_S.Location = new System.Drawing.Point(146, 23);
+      this.txt_S.Name = "txt_S";
+      this.txt_S.Size = new System.Drawing.Size(50, 20);
+      this.txt_S.TabIndex = 1;
+      this.txt_S.Text = "3000";
+      // 
+      // label10
+      // 
+      this.label10.AutoSize = true;
+      this.label10.Location = new System.Drawing.Point(6, 23);
+      this.label10.Name = "label10";
+      this.label10.Size = new System.Drawing.Size(121, 13);
+      this.label10.TabIndex = 0;
+      this.label10.Text = "Обороты шпинделя - S";
+      // 
+      // but_close
+      // 
+      this.but_close.Location = new System.Drawing.Point(320, 676);
+      this.but_close.Name = "but_close";
+      this.but_close.Size = new System.Drawing.Size(75, 23);
+      this.but_close.TabIndex = 9;
+      this.but_close.Text = "Закрыть";
+      this.but_close.UseVisualStyleBackColor = true;
+      this.but_close.Click += new System.EventHandler(this.but_close_Click);
+      // 
+      // frm_edit_prog
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(414, 706);
+      this.Controls.Add(this.but_close);
+      this.Controls.Add(this.groupBox2);
+      this.Controls.Add(this.but_edit);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.but_save);
+      this.Controls.Add(this.txt_box_prog);
+      this.Controls.Add(this.lab_lst_prog);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.KeyPreview = true;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "frm_edit_prog";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Редактор программ сверления";
+      this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frm_edit_prog_KeyDown);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.groupBox2.ResumeLayout(false);
+      this.groupBox2.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+    private System.Windows.Forms.Label lab_lst_prog;
+    private System.Windows.Forms.TextBox txt_box_prog;
+    private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    private System.Windows.Forms.Button but_save;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox txt_H;
+    private System.Windows.Forms.TextBox txt_G;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox txt_H2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox txt_H1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Button but_edit;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.TextBox txt_XY;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.TextBox txt_D;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.TextBox txt_Z2;
+    private System.Windows.Forms.Label label7;
+    private System.Windows.Forms.TextBox txt_Z1;
+    private System.Windows.Forms.Label label8;
+    private System.Windows.Forms.TextBox txt_F;
+    private System.Windows.Forms.Label label9;
+    private System.Windows.Forms.TextBox txt_S;
+    private System.Windows.Forms.Label label10;
+    private System.Windows.Forms.Button but_close;
+  }
+}
+
---
+++ ProEditor/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ProEditor
+{
+  public partial class frm_edit_prog : Form
+  {
+    bool flg_err_msg = false;
+    public frm_edit_prog()
+    {
+      InitializeComponent();
+      saveFileDialog1.Filter = "Text files(*.NC)|*.nc|All files(*.*)|*.*";
+      txt_G.LostFocus += is_check_fld;
+      txt_H.LostFocus += is_check_fld;
+      txt_H1.LostFocus += is_check_fld;
+      txt_H2.LostFocus += is_check_fld;
+      txt_XY.LostFocus += is_check_fld;
+      txt_S.LostFocus += is_check_fld;
+      txt_F.LostFocus += is_check_fld;
+      txt_Z1.LostFocus += is_check_fld;
+      txt_Z2.LostFocus += is_check_fld;
+    }
+
+    //сохранить файл
+    private void but_save_Click(object sender, EventArgs e)
+    {
+      if (txt_box_prog.Text == "")
+      {
+        MessageBox.Show("Нет данных для сохранения!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        return;
+      }     
+      if (saveFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+
+      string file_name = saveFileDialog1.FileName;
+      System.IO.File.WriteAllText(file_name, txt_box_prog.Text);
+    }
+
+    //закрыть по Esc
+    private void frm_edit_prog_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.KeyCode == Keys.Escape) Close();
+    }
+
+    //сгенерировать программу
+    private void but_edit_Click(object sender, EventArgs e)
+    {
+      string txt_prog = "", txt_prog_buf = "", fg1 ="";
+      int poz=0, num = 30, h2=0, i=0, j=0, h1=0, hg=0, g=0, g1=0;
+      int s = Int32.Parse(txt_S.Text); //значение оборотов шпинделя на форме должно реальное, потом его надо умножить на 2 - особенность стойки Esfero 3D
+
+      String[] xy_list= txt_XY.Text.Split(new String[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+      h1 = Int32.Parse(txt_H1.Text);
+      h2 = Int32.Parse(txt_H2.Text);
+      g = Int32.Parse(txt_G.Text);
+
+      //шапка программы
+      string txt_start = "(StepMOTOReN - Esfero 3D)\r\n(TOOL 2 - DIA " + txt_D.Text + " DRILL)\r\n";
+      txt_start += "N5 G90 G17 G40 G80 G00\r\nN10 M06 T2\r\nN15 S"+ Convert.ToString(s*2) + " M03\r\n";
+      txt_start += "N20 G00 G90 Z" + txt_Z1.Text + "\r\nN25 Z10\r\n";
+
+      char[] charsToTrim = { '.', ' '};
+      hg = g / h1 + 1;
+      for (i=0; i< xy_list.Length; i++){
+        txt_prog_buf = xy_list[i].Trim(charsToTrim);
+        if (txt_prog_buf.StartsWith("N"))
+        {
+          poz = txt_prog_buf.IndexOf(" ", 0);
+          txt_prog_buf = txt_prog_buf.Substring(poz + 1);
+        }
+
+        txt_prog += "N" + Convert.ToString(num) + " " + txt_prog_buf + "\r\n";
+        for (j=1; j<= hg; j++)
+        {
+          if (j == 1) fg1 = " F" + txt_F.Text; else fg1 = "";
+          g1 = h1 * j;
+          if (g - g1 < 0) g1 = g;
+          txt_prog += "N" + Convert.ToString(num + 5) + " G01 Z-" + Convert.ToString(g1) + fg1 + "\r\n";
+          if (g1 == g) break;
+          if (j < hg)
+          {
+            txt_prog += "N" + Convert.ToString(num + 10) + " G00 Z" + txt_H.Text + "\r\n";
+            txt_prog += "N" + Convert.ToString(num + 15) + " Z-" + Convert.ToString(h1 * j - h2) + "\r\n";
+          }
+          num += 15;
+        }
+        if(i < xy_list.Length-1)
+          txt_prog += "N" + Convert.ToString(num-5) + " G00 Z" + txt_H.Text + "\r\n";
+      }
+
+      //конец программы
+      string txt_end = "N" + Convert.ToString(num + 5) + " G00 Z" + txt_Z2.Text + "\r\n";
+      txt_end += "N" + Convert.ToString(num + 10) + " G80\r\n";
+      txt_end += "N" + Convert.ToString(num + 15) + " M05\r\n";
+      txt_end += "N" + Convert.ToString(num + 20) + " M30";
+
+      //итоговая сборка
+      txt_box_prog.Text = txt_start + txt_prog + txt_end;
+    }
+
+    //проверки после изменения значений
+    private void is_check_fld(object sender, EventArgs e)
+    {
+      int val1 = 0, val2 = 0;
+      
+      TextBox txt_fld = (TextBox)sender;
+      //проверка на пустые поля
+      if (txt_fld.Text == "")
+      {
+        MessageBox.Show("Поле не должно быть пустым!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        txt_fld.Undo();
+        txt_fld.Focus();
+        return;
+      }
+
+      //проверка свойств чисел
+      if(txt_fld.Name != "txt_XY")
+      {
+        //проверка является ли значение в поле числом
+        bool result = int.TryParse(txt_fld.Text, out val1);
+        if (result == false)
+        {
+          MessageBox.Show("Значение " + txt_fld.Text + " не является числом!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+          txt_fld.Undo();
+          txt_fld.Focus();
+          return;
+        }
+        //проверка на ноль и отрицательное значение
+        val1 = Int32.Parse(txt_fld.Text);
+        if (val1 <= 0)
+        {
+          MessageBox.Show("Значение " + txt_fld.Text + " не может быть меньше или равным нулю!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+          txt_fld.Undo();
+          txt_fld.Focus();
+          return;
+        }
+      }
+
+      //проверка оборотов шпинделя
+      if (txt_fld.Name == "txt_S")
+      {
+        if (val1 < 3000)
+        {
+          MessageBox.Show("Обороты шпинделя не могут быть меньше 3000!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+          txt_fld.Undo();
+          txt_fld.Focus();
+          return;
+        }
+        if (val1 > 12000)
+        {
+          MessageBox.Show("Обороты шпинделя не могут быть больше 12000!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+          txt_fld.Undo();
+          txt_fld.Focus();
+          return;
+        }
+      }
+
+      //проверка глубины и недохода
+      if (txt_fld.Name == "txt_H1" || txt_fld.Name == "txt_H2")
+      {
+        if(flg_err_msg==true)
+        {
+          flg_err_msg = false;
+          return;
+        }
+        val1 = Int32.Parse(txt_H1.Text);
+        val2 = Int32.Parse(txt_H2.Text);
+        if (val1 <= val2)
+        {
+          flg_err_msg = true;
+          MessageBox.Show("Значение Н1 не может быть меньше либо равно Н2!", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+          txt_fld.Undo();
+          txt_fld.Focus();
+          return;
+        }
+      }
+    }
+
+    private void but_close_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+
+    private void groupBox1_Enter(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
