baea9a58ad949970e0df91c78d127d3c49fdbdd4
Алексей Парусников <instrpro@mail.ru>
2022-07-27 15:16:51 +0600

    Изменение макета формы

--- ProEditor/Form1.Designer.cs
+++ ProEditor/Form1.Designer.cs
@@ -71,7 +71,7 @@ namespace ProEditor
       // label14
       // 
       this.label14.AutoSize = true;
-      this.label14.Location = new System.Drawing.Point(18, 402);
+      this.label14.Location = new System.Drawing.Point(9, 367);
       this.label14.Name = "label14";
       this.label14.Size = new System.Drawing.Size(99, 13);
       this.label14.TabIndex = 2;
@@ -79,11 +79,11 @@ namespace ProEditor
       // 
       // txt_box_prog
       // 
-      this.txt_box_prog.Location = new System.Drawing.Point(12, 418);
+      this.txt_box_prog.Location = new System.Drawing.Point(12, 383);
       this.txt_box_prog.Multiline = true;
       this.txt_box_prog.Name = "txt_box_prog";
       this.txt_box_prog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-      this.txt_box_prog.Size = new System.Drawing.Size(383, 252);
+      this.txt_box_prog.Size = new System.Drawing.Size(383, 287);
       this.txt_box_prog.TabIndex = 4;
       // 
       // but_save
@@ -98,8 +98,6 @@ namespace ProEditor
       // 
       // groupBox1
       // 
-      this.groupBox1.Controls.Add(this.txt_XY);
-      this.groupBox1.Controls.Add(this.label5);
       this.groupBox1.Controls.Add(this.txt_G);
       this.groupBox1.Controls.Add(this.label4);
       this.groupBox1.Controls.Add(this.txt_H2);
@@ -111,25 +109,25 @@ namespace ProEditor
       this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
       this.groupBox1.Location = new System.Drawing.Point(12, 12);
       this.groupBox1.Name = "groupBox1";
-      this.groupBox1.Size = new System.Drawing.Size(393, 132);
+      this.groupBox1.Size = new System.Drawing.Size(205, 132);
       this.groupBox1.TabIndex = 6;
       this.groupBox1.TabStop = false;
       this.groupBox1.Text = "Параметры цикла G83";
       // 
       // txt_XY
       // 
-      this.txt_XY.Location = new System.Drawing.Point(216, 42);
+      this.txt_XY.Location = new System.Drawing.Point(228, 35);
       this.txt_XY.Multiline = true;
       this.txt_XY.Name = "txt_XY";
       this.txt_XY.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-      this.txt_XY.Size = new System.Drawing.Size(167, 79);
+      this.txt_XY.Size = new System.Drawing.Size(167, 94);
       this.txt_XY.TabIndex = 9;
       this.txt_XY.Text = "X100 Y-200\r\nX-100 Y200";
       // 
       // label5
       // 
       this.label5.AutoSize = true;
-      this.label5.Location = new System.Drawing.Point(215, 26);
+      this.label5.Location = new System.Drawing.Point(223, 12);
       this.label5.Name = "label5";
       this.label5.Size = new System.Drawing.Size(86, 13);
       this.label5.TabIndex = 8;
@@ -137,7 +135,7 @@ namespace ProEditor
       // 
       // txt_G
       // 
-      this.txt_G.Location = new System.Drawing.Point(146, 101);
+      this.txt_G.Location = new System.Drawing.Point(146, 97);
       this.txt_G.Name = "txt_G";
       this.txt_G.Size = new System.Drawing.Size(50, 20);
       this.txt_G.TabIndex = 7;
@@ -154,7 +152,7 @@ namespace ProEditor
       // 
       // txt_H2
       // 
-      this.txt_H2.Location = new System.Drawing.Point(146, 75);
+      this.txt_H2.Location = new System.Drawing.Point(146, 71);
       this.txt_H2.Name = "txt_H2";
       this.txt_H2.Size = new System.Drawing.Size(50, 20);
       this.txt_H2.TabIndex = 5;
@@ -171,7 +169,7 @@ namespace ProEditor
       // 
       // txt_H1
       // 
-      this.txt_H1.Location = new System.Drawing.Point(146, 49);
+      this.txt_H1.Location = new System.Drawing.Point(146, 45);
       this.txt_H1.Name = "txt_H1";
       this.txt_H1.Size = new System.Drawing.Size(50, 20);
       this.txt_H1.TabIndex = 3;
@@ -188,7 +186,7 @@ namespace ProEditor
       // 
       // txt_H
       // 
-      this.txt_H.Location = new System.Drawing.Point(146, 23);
+      this.txt_H.Location = new System.Drawing.Point(146, 19);
       this.txt_H.Name = "txt_H";
       this.txt_H.Size = new System.Drawing.Size(50, 20);
       this.txt_H.TabIndex = 1;
@@ -197,7 +195,7 @@ namespace ProEditor
       // label1
       // 
       this.label1.AutoSize = true;
-      this.label1.Location = new System.Drawing.Point(6, 26);
+      this.label1.Location = new System.Drawing.Point(8, 26);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(119, 13);
       this.label1.TabIndex = 0;
@@ -235,11 +233,7 @@ namespace ProEditor
       this.groupBox2.Controls.Add(this.label13);
       this.groupBox2.Controls.Add(this.txt_Step);
       this.groupBox2.Controls.Add(this.label6);
-      this.groupBox2.Controls.Add(this.label12);
       this.groupBox2.Controls.Add(this.txt_D);
-      this.groupBox2.Controls.Add(this.txt_End);
-      this.groupBox2.Controls.Add(this.label11);
-      this.groupBox2.Controls.Add(this.txt_Start);
       this.groupBox2.Controls.Add(this.txt_Z2);
       this.groupBox2.Controls.Add(this.label7);
       this.groupBox2.Controls.Add(this.txt_Z1);
@@ -251,7 +245,7 @@ namespace ProEditor
       this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
       this.groupBox2.Location = new System.Drawing.Point(12, 150);
       this.groupBox2.Name = "groupBox2";
-      this.groupBox2.Size = new System.Drawing.Size(393, 249);
+      this.groupBox2.Size = new System.Drawing.Size(205, 214);
       this.groupBox2.TabIndex = 8;
       this.groupBox2.TabStop = false;
       this.groupBox2.Text = "Параметры программы";
@@ -276,7 +270,7 @@ namespace ProEditor
       // label12
       // 
       this.label12.AutoSize = true;
-      this.label12.Location = new System.Drawing.Point(213, 143);
+      this.label12.Location = new System.Drawing.Point(223, 269);
       this.label12.Name = "label12";
       this.label12.Size = new System.Drawing.Size(84, 13);
       this.label12.TabIndex = 13;
@@ -284,7 +278,7 @@ namespace ProEditor
       // 
       // txt_End
       // 
-      this.txt_End.Location = new System.Drawing.Point(216, 159);
+      this.txt_End.Location = new System.Drawing.Point(228, 285);
       this.txt_End.Multiline = true;
       this.txt_End.Name = "txt_End";
       this.txt_End.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -295,7 +289,7 @@ namespace ProEditor
       // label11
       // 
       this.label11.AutoSize = true;
-      this.label11.Location = new System.Drawing.Point(213, 7);
+      this.label11.Location = new System.Drawing.Point(225, 150);
       this.label11.Name = "label11";
       this.label11.Size = new System.Drawing.Size(91, 13);
       this.label11.TabIndex = 11;
@@ -303,11 +297,11 @@ namespace ProEditor
       // 
       // txt_Start
       // 
-      this.txt_Start.Location = new System.Drawing.Point(216, 22);
+      this.txt_Start.Location = new System.Drawing.Point(228, 165);
       this.txt_Start.Multiline = true;
       this.txt_Start.Name = "txt_Start";
       this.txt_Start.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-      this.txt_Start.Size = new System.Drawing.Size(167, 117);
+      this.txt_Start.Size = new System.Drawing.Size(167, 98);
       this.txt_Start.TabIndex = 10;
       this.txt_Start.Text = "(StepMOTOReN - Esfero 3D)\r\n(TOOL 2 - DIA[D] DRILL)\r\nG90 G17 G40 G80 G00\r\nM06 T2\r\n" +
     "S[S] M03\r\nG00 G90 Z[Z1]\r\nZ10";
@@ -394,10 +388,16 @@ namespace ProEditor
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(410, 706);
+      this.ClientSize = new System.Drawing.Size(400, 706);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.txt_XY);
       this.Controls.Add(this.but_close);
+      this.Controls.Add(this.label12);
+      this.Controls.Add(this.label11);
       this.Controls.Add(this.groupBox2);
+      this.Controls.Add(this.txt_Start);
       this.Controls.Add(this.but_edit);
+      this.Controls.Add(this.txt_End);
       this.Controls.Add(this.groupBox1);
       this.Controls.Add(this.but_save);
       this.Controls.Add(this.txt_box_prog);
